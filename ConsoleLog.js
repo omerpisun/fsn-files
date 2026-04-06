5d85480e49f2a4119689e4b71f8e3dd2fdf3d2bc
omerpisun <omerpisun@gmail.com>
2026-04-06 03:30:07 +0300

    ConsoleLog.js

--- ConsoleLog.js
+++ ConsoleLog.js
@@ -12,72 +12,55 @@ var ConsoleLog = {
               class: 'terminal'
           }).append($('<div/>', {
               class: 'terminal-output'
-          })).on('scroll', function () {
-              ConsoleLog.LogScrollBottom();
-          }));
+          })).on('scroll', () => this.LogScrollBottom()));
 
         var output = container.find('.terminal-output');
 
-        $.each(this.Logs, function (_, log) {
-            output.append(log);
-        });
+        this.Logs.forEach(log => output.append(log));
 
         return container;
     },
 
     Log: function (message, type) {
-        if (this.Logs.length >= 500) {
-            this.Logs.shift();
-        }
+        if (this.Logs.length >= 500) this.Logs.shift();
 
-        function pad(n) {
-            return n < 10 ? '0' + n : n;
-        }
+        const now = new Date();
+        const time = now.toLocaleTimeString();
 
-        var now = new Date();
-        var time = pad(now.getHours()) + ':' + pad(now.getMinutes()) + ':' + pad(now.getSeconds());
+        const logType = this.Types[type] || 'Log';
 
-        var logType = this.Types[type] || 'Log';
-
-        var entry = $('<div/>').append(
-            $('<div/>', {
-                style: 'width:100%;'
-            }).html(time + ' - [' + logType + ']: ' + message)
+        const entry = $('<div/>').append(
+            $('<div/>', { style: 'width:100%;' })
+            .html(`${time} - [${logType}]: ${message}`)
         );
 
         this.Logs.push(entry);
 
-        var output = $('.terminal-output');
+        const output = $('.terminal-output');
+        if (!output.length) return;
 
-        if (output.length) {
-            output.append(entry);
+        output.append(entry);
 
-            if (this.scrollUpdate) {
-                var terminal = $('.terminal');
-                var scrollHeight = output[0].scrollHeight;
-                terminal.scrollTop(scrollHeight);
-            }
+        if (this.scrollUpdate) {
+            $('.terminal').scrollTop(output[0].scrollHeight);
         }
     },
 
     LogScrollBottom: function () {
         clearInterval(this.scrollInterval);
 
-        var terminal = $('.terminal');
-        var output = $('.terminal-output');
+        const terminal = $('.terminal');
+        const output = $('.terminal-output');
 
         if (!terminal.length || !output.length) return;
 
-        if (terminal.scrollTop() + terminal.innerHeight() >= output[0].scrollHeight - 1) {
-            this.scrollUpdate = true;
-        } else {
-            this.scrollUpdate = false;
-        }
+        this.scrollUpdate =
+            terminal.scrollTop() + terminal.innerHeight() >= output[0].scrollHeight - 1;
 
-        var scrollHeight = output[0].scrollHeight;
+        const scrollHeight = output[0].scrollHeight;
 
-        this.scrollInterval = setInterval(function () {
+        this.scrollInterval = setInterval(() => {
             terminal.scrollTop(scrollHeight);
-        }, 7000);
+        }, 5000);
     }
 };

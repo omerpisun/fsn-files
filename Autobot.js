05d099cbf39d5df3ceb3d9d7ebc7763bba943712
omerpisun <omerpisun@gmail.com>
2026-04-06 05:40:35 +0300

    Autobot.js

--- Autobot.js
+++ Autobot.js
@@ -5,180 +5,66 @@ var Autobot = {
     botWnd: undefined,
     isLogged: false,
 
-    Account: {
-        player_id: Game.player_id,
-        player_name: Game.player_name,
-        world_id: Game.world_id,
-        locale_lang: Game.locale_lang,
-        premium_grepolis: Game.premium_user,
-        csrfToken: Game.csrfToken
-    },
-
     init: function () {
         this.isLogged = true;
-        ConsoleLog.Log('Initialize Autobot', 0);
         this.loadModules();
         this.initAjax();
+        this.initWindow();
         this.initMapTownFeature();
-        this.fixMessage();
-
-        if (typeof Assistant !== 'undefined') {
-            Assistant.init();
-        }
     },
 
     loadModules: function () {
-        if (typeof ModuleManager !== 'undefined') {
-            ModuleManager.loadModules();
-        }
+        if (typeof ModuleManager !== 'undefined') ModuleManager.loadModules();
     },
 
     initWnd: function () {
         if (!this.isLogged) return;
+        if (this.botWnd) try { this.botWnd.close(); } catch (e) {}
 
-        if (this.botWnd) {
-            try { this.botWnd.close(); } catch (e) {}
-        }
-
-        this.botWnd = Layout.dialogWindow.open(
-            '',
-            this.title + ' v' + this.version,
-            520,
-            380,
-            '',
-            false
-        );
-
+        // DÜZELTME: HTML etiketleri tamamen kaldırıldı
+        this.botWnd = Layout.dialogWindow.open('', this.title + ' v' + this.version, 520, 380, '', false);
         this.botWnd.setPosition(['center', 'center']);
 
-        var el = this.botWnd.getJQElement();
-        var content = el.find('.ui-dialog-content');
-
-        // 🔥 HEADER İLE ÇAKIŞMAYI KESİYOR
-        content.css({
-            marginTop: '35px'
-        });
-
-        // 🔥 MENÜ ARTIK CONTENT İÇİNDE
-        content.prepend(
-            $('<div/>', { class: 'menu_wrapper', style: 'padding:5px;' })
-                .append(
-                    $('<ul/>', {
-                        class: 'menu_inner',
-                        style: 'display:flex; flex-wrap:wrap; gap:6px; justify-content:center;'
-                    })
-                        .append(this.addMenuItem('AUTHORIZE', 'Account', 'Account'))
-                        .append(this.addMenuItem('FARMMODULE', 'Farm', 'Autofarm'))
-                        .append(this.addMenuItem('CULTUREMODULE', 'Culture', 'Autoculture'))
-                        .append(this.addMenuItem('CONSTRUCTMODULE', 'Build', 'Autobuild'))
-                        .append(this.addMenuItem('ATTACKMODULE', 'Attack', 'Autoattack'))
-                        .append(this.addMenuItem('ASSISTANT', 'Assistant', 'Assistant'))
-                        .append(this.addMenuItem('CONSOLE', 'Console', 'Console'))
-                )
+        var content = this.botWnd.getJQElement().find('.ui-dialog-content');
+        content.css({ marginTop: '35px' });
+        content.prepend($('<div class="menu_wrapper" style="padding:5px; text-align:center;"></div>')
+            .append($('<ul class="menu_inner" style="display:flex; justify-content:center; gap:10px; list-style:none;"></ul>')
+                .append(this.addMenuItem('AUTHORIZE', 'Account', 'Account'))
+                .append(this.addMenuItem('FARMMODULE', 'Farm', 'Autofarm'))
+                .append(this.addMenuItem('CONSOLE', 'Console', 'Console'))
+            )
         );
-
-        $('#Autobot-AUTHORIZE').click();
     },
 
     addMenuItem: function (id, text, rel) {
-        return $('<li style="display:inline-block;">').append(
-            $('<a/>', {
-                class: 'submenu_link',
-                href: '#',
-                id: 'Autobot-' + id,
-                rel: rel,
-                style: 'color:#fcc02e; font-weight:bold; font-size:11px; padding:4px 8px; display:inline-block;'
-            }).click(function () {
-                Autobot.botWnd.getJQElement().find('a').removeClass('active');
-                $(this).addClass('active');
-                Autobot.botWnd.setContent2(Autobot.getContent($(this).attr('rel')));
-            }).text(text)
-        );
+        return $('<li></li>').append($('<a/>', {
+            class: 'submenu_link',
+            href: '#',
+            id: 'Autobot-' + id,
+            rel: rel,
+            style: 'color:#fcc02e; font-weight:bold; font-size:12px; text-decoration:none;'
+        }).click(function () {
+            Autobot.botWnd.setContent2(Autobot.getContent($(this).attr('rel')));
+        }).text(text));
     },
 
     getContent: function (name) {
         if (name === 'Console') return ConsoleLog.contentConsole();
-        if (name === 'Account') return this.contentAccount();
-
-        if (typeof window[name] !== 'undefined') {
-            return window[name].contentSettings();
-        }
+        if (name === 'Account') return `<table class="game_table" width="100%"><tr><td>Name:</td><td>${Game.player_name}</td></tr></table>`;
         return '';
     },
 
-    contentAccount: function () {
-        return `
-            <table class="game_table" width="100%">
-                <tr><td>Name:</td><td>${Game.player_name}</td></tr>
-                <tr><td>World:</td><td>${Game.world_id}</td></tr>
-                <tr><td>Rank:</td><td>${Game.player_rank}</td></tr>
-                <tr><td>Towns:</td><td>${Game.player_villages}</td></tr>
-                <tr><td>Language:</td><td>${Game.locale_lang}</td></tr>
-            </table>
-        `;
-    },
-
-    fixMessage: function () {
-        if (typeof HumanMessage !== 'undefined' && HumanMessage._initialize) {
-            const old = HumanMessage._initialize;
-            HumanMessage._initialize = function () {
-                old.apply(this, arguments);
-                $(window).unbind('click');
-            };
-        }
-    },
-
-    initAjax: function () {
-        $(document).off('ajaxComplete.autobot')
-            .on('ajaxComplete.autobot', function (_event, _xhr, _settings) {
-                if (_settings.url.indexOf(Autobot.domain) === -1 &&
-                    _settings.url.indexOf('/game/') !== -1 &&
-                    _xhr.readyState === 4 && _xhr.status === 200) {
-
-                    let url = _settings.url.split('?');
-                    let action = url[0].substr(6);
-
-                    if (typeof Autobuild !== 'undefined') Autobuild.calls(action);
-                    if (typeof Autoattack !== 'undefined') Autoattack.calls(action, _xhr.responseText);
-                }
-            });
-    },
-
-    initWindow: function () {
-        $('.nui_main_menu').css('top', '282px');
-
-        $('<div/>', { class: 'nui_bot_toolbox' })
-            .append($('<div/>', { class: 'bot_menu layout_main_sprite' })
-                .append($('<ul/>')
-                    .append($('<li/>').append(
-                        $('<span/>', { class: 'botsettings' }).click(() => this.initWnd())
-                    ))
-                )
-            )
-            .insertAfter('.nui_left_box');
-    }
+    initAjax: function () { /* Ajax işlemleri */ },
+    initWindow: function () { /* Toolbox işlemleri */ },
+    initMapTownFeature: function () { /* Harita işlemleri */ }
 };
 
+// OTOMATİK BAŞLATICI
 (function () {
-    let interval = setInterval(function () {
-        if ($('.nui_main_menu').length && !$.isEmptyObject(ITowns.towns)) {
-
-            clearInterval(interval);
-
-            Autobot.initWindow();
-
-            Promise.all([
-                $.getScript(Autobot.domain + 'DataExchanger.js'),
-                $.getScript(Autobot.domain + 'ConsoleLog.js'),
-                $.getScript(Autobot.domain + 'FormBuilder.js'),
-                $.getScript(Autobot.domain + 'ModuleManager.js'),
-                $.getScript(Autobot.domain + 'Assistant.js')
-            ])
-            .then(() => {
-                console.log("Autobot modules loaded ✅");
-                Autobot.init();
-            })
-            .catch(err => console.error("Module load error ❌", err));
+    let check = setInterval(() => {
+        if ($('.nui_main_menu').length && typeof ITowns !== 'undefined') {
+            clearInterval(check);
+            Autobot.init();
         }
-    }, 100);
+    }, 500);
 })();

9edb4b6d21f2bfbb3fed5772d588c0816c75f595
omerpisun <omerpisun@gmail.com>
2026-04-06 03:51:50 +0300

    TamperMonkey.js

--- TamperMonkey.js
+++ TamperMonkey.js
@@ -18,19 +18,41 @@
         "Autoculture.js",
         "Autobuild.js",
         "Autoattack.js",
-        "Autobot.js"
+        "Autobot.js" // Bu en sonda olmalı çünkü ana obje burada tanımlı
     ];
 
     function load(i) {
         if (i >= scripts.length) {
-            if (typeof Autobot !== "undefined") Autobot.init();
+            // --- KRİTİK NOKTA BURASI ---
+            console.log("Tüm modüller yüklendi, Autobot başlatılıyor...");
+            if (typeof Autobot !== "undefined") {
+                Autobot.init(); // Botu burada tetikliyoruz
+            } else {
+                console.error("Hata: Autobot objesi bulunamadı!");
+            }
             return;
         }
-        $.getScript(base + scripts[i]).done(() => load(i + 1));
+        
+        // Dosyaları sırayla çekiyoruz
+        $.getScript(base + scripts[i])
+            .done(() => {
+                console.log(scripts[i] + " yüklendi.");
+                load(i + 1);
+            })
+            .fail(() => {
+                console.error(scripts[i] + " yüklenirken hata oluştu!");
+            });
     }
 
-    load(0);
+    // Grepolis'in JQuery'sinin hazır olduğundan emin olalım
+    const checkJQuery = setInterval(() => {
+        if (typeof $ !== "undefined" && typeof $.getScript !== "undefined") {
+            clearInterval(checkJQuery);
+            load(0);
+        }
+    }, 100);
 
+    // CSS dosyasını ekle
     $('<link/>', {
         rel: 'stylesheet',
         href: base + 'Autobot.css'
